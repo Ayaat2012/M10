2f60e808a914b0fd5e0dfe1c27ba07a2996087e3
Ayaat2012 <iffatrajshahi@gmail.com>
2025-09-26 19:44:16 +0600

    initial commit

---
+++ CWs/L2-A1.py
@@ -0,0 +1,11 @@
+#Program to show Time Compexity
+
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is ", n)
+    iteration+=1
+    print("Total iteration done by the code is ", iteration, "\n")
+
+printnumber(10)
+printnumber(15)
+print("\nWith any 'n', the time taken by our code won't change.")
